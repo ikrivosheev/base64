d427c954a4e172ff46d4d83b5c5a5a0473592995
Ivan Krivosheev <py.krivosheev@gmail.com>
2020-01-22 02:08:18 +0300

    Fix tests

--- tests/test_decode.c
+++ tests/test_decode.c
@@ -4,15 +4,20 @@
 #include "tests.h"
 
 static bool assert_decode(
-    const char* src, 
+    const char** chunks,
+    const char chunks_len,
     const char* expected, 
     const size_t expected_len) 
 {
+    int i;
     struct b64_decode_state state;
     char result[100]; 
 
     b64_stream_decode_init(&state);
-    b64_stream_decode(&state, src, 4, result);
+    for (i = 0; i < chunks_len; i++)
+    {
+        b64_stream_decode(&state, chunks[i], strlen(chunks[i]), result);
+    }
 
     ASSERT(b64_stream_decode_final(&state), "b64_stream_decode_final");
     ASSERT(expected_len == state.out_len, "Decoded result length is not equal expected");
@@ -22,22 +27,26 @@ static bool assert_decode(
 
 bool test_decode_simple()
 {
-    return assert_decode("QUFB", "AAA", 3);
+    const char* chunks[] = {"QUFB"};
+    return assert_decode(chunks, 1, "AAA", 3);
 }
 
 bool test_decode_padding()
 {
-    return assert_decode("QUE=", "AA", 2);
+    const char* chunks[] = {"QUE="};
+    return assert_decode(chunks, 1, "AA", 2);
 }
 
 bool test_decode_big_padding()
 {
-    return assert_decode("QQ==", "A", 1);
+    const char* chunks[] = {"QQ=="};
+    return assert_decode(chunks, 1, "A", 1);
 }
 
 bool test_decode_with_newline()
 {
-    return assert_decode("Q\nQ==", "A", 1);
+    const char* chunks[] = {"Q\nQ=="};
+    return assert_decode(chunks, 1, "A", 1);
 }
 
 int main()
--- tests/test_encode.c
+++ tests/test_encode.c
@@ -5,17 +5,21 @@
 
 
 static bool assert_encode(
-    const char* src,
-    const int src_len,
+    const char** chunks,
+    const int chunks_len,
     const char* expected,
     const size_t expected_len
 )
 {
+    int i;
     struct b64_encode_state state;
     char result[100];
 
     b64_stream_encode_init(&state);
-    b64_stream_encode(&state, src, src_len, result);
+    for (i = 0; i < chunks_len; i++)
+    {
+        b64_stream_encode(&state, chunks[i], strlen(chunks[i]), result);
+    }
     
     ASSERT(b64_stream_encode_final(&state, result), "b64_stream_encode_final");
     ASSERT(expected_len == state.out_len, "Encoded result length is not equal expected");
@@ -25,12 +29,14 @@ static bool assert_encode(
 
 bool test_encode_simple()
 {
-    return assert_encode("AAA", 3, "QUFB", 4);
+    const char* chunks[] = {"AAA"};
+    return assert_encode(chunks, 1, "QUFB", 4);
 }
 
 bool test_encode_with_padding()
 {
-    return assert_encode("AA", 2, "QUE=", 4);
+    const char* chunks[] = {"AA"};
+    return assert_encode(chunks, 1, "QUE=", 4);
 }
 
 
