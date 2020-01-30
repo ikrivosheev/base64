daa78b435ec208d67b6c48c5ffbb795f5a9b1d7f
Ivan Krivosheev <py.krivosheev@gmail.com>
2020-01-31 00:20:44 +0300

    Add CLI for bin file

--- bin/base64.c
+++ bin/base64.c
@@ -8,6 +8,8 @@
 #define B64_ENCODE_BUFFER_SIZE (1024 + 2) * 4 / 3
 #define B64_DECODE_BUFFER_SIZE 1024 / 4 * 3
 
+typedef int (*handler_t)(FILE*);
+
 
 int encode(FILE* file) 
 {
@@ -31,13 +33,14 @@ int encode(FILE* file)
     
     int buffer_len = b64_stream_encode_final(&state, buffer);
     fwrite(buffer, buffer_len, 1, stdout);
-
+    fwrite("\n", 1, 1, stdout);
     return 0;
 
 }
 
 int decode(FILE* file)
 {
+    int result;
     int read_len;
     char read_buffer[B64_READ_BUFFER_SIZE];
     char buffer[B64_ENCODE_BUFFER_SIZE];
@@ -54,18 +57,62 @@ int decode(FILE* file)
         if (feof(file))
             break;
     }
-
-    return b64_stream_decode_final(&state);
+    
+    result = b64_stream_decode_final(&state);
+    if (!result)
+    {
+        printf("base64: invalid input\n");
+    }
+    return result;
 }
 
-int main() 
+void help() 
 {
-    FILE* file = stdin;
+    printf("Usage: base64 [OPTION]... [FILE]\n");
+    printf("Base64 encode or decode file or standart input to standart output\n"); 
+    printf("\n");
+    printf("Mandatory arguments to long options are mandatory for short options too.\n");
+    printf("  -d, --decode  decode data\n");
+    printf("  -h, --help    display this help and exit\n");
+}
 
-    int result = encode(file);
+int main(int argc, char* argv[]) 
+{
+    int i = 1;
+    int decode_flag = 0;
+    FILE* fd = stdin;
+    handler_t handler = &encode;
+    
+    while (1)
+    {
+        if (i == argc)
+            break;
+        if ((strcmp(argv[i], "-d") == 0) || (strcmp(argv[i], "--decode") == 0))
+        {
+            i++;
+            handler = &decode;
+            continue;
+        }
+        else if ((strcmp(argv[i], "-h") == 0) || (strcmp(argv[i], "--help") == 0))
+        {
+            help();
+            return 0;
+        }
+        else
+        {
+            fd = fopen(argv[i], "rb");
+            i++;
+            if (fd == NULL)
+            {
+                printf("base64: %s: No such file or directory\n", argv[i]);
+                return 1;
+            }
+        }
+    }
 
-    fwrite("\n", 1, 1, stdout);
+    int result = handler(fd);
+
     fflush(stdout);
-    fclose(file);
+    fclose(fd);
     return result;
 }
